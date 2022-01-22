c1e589834bceb62ff3f96dfad4b909e38bd2099f
harshgoel05 <harshgoel05@gmail.com>
2022-01-22 12:35:20 +0530

    ♻️ Refactor api

--- src/shared/database.ts
+++ src/shared/database.ts
@@ -4,9 +4,12 @@ import config from '../config';
 let db: Db;
 
 async function initializeClient(): Promise<Db> {
-  const client = await MongoClient.connect(config.databaseURL);
-
-  return client.db();
+  try {
+    const client = await MongoClient.connect(config.databaseURL);
+    return client.db();
+  } catch (err) {
+    console.log('Error connecting to database!');
+  }
 }
 
 export default async (): Promise<Db> => {
